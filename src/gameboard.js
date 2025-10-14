41d270c1723b52d4e7faf720f61e417ddc960a78
Luke Sullivan <luke@sukelully.dev>
2025-10-14 20:56:29 +0100

    refactor: get placeShip working correctly

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,17 +2,25 @@ import { Ship } from "./ship";
 
 export class Gameboard {
   #size = 10;
-  board = Array.from({ length: this.#size }, () => Array(this.#size).fill(null));
+  #board = Array.from({ length: this.#size }, () => Array(this.#size).fill(null));
+
+  getCell(x, y) {
+    return this.#board[x][y];
+  }
 
   placeShip(x, y, length, isVertical = false) {
     const ship = new Ship(length);
 
     for (let i = 0; i < length; i++) {
       if (isVertical) {
-        this.board[y + i][x] = ship;
+        this.#board[y + i][x] = ship;
       } else {
-        this.board[y][x + i] = ship;
+        this.#board[y][x + i] = ship;
       }
     }
+  }
+
+  receiveAttack(x, y) {
+    
   }
 }
