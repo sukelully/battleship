aefcabdf52f4ffb216d7077f07a5656a41d4a999
Luke Sullivan <luke@sukelully.dev>
2025-10-16 12:32:16 +0100

    style: update ui layout

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import './style.css';
-// import { Gameboard } from "./models/gameboard";
-// import { Ship } from "./models/ship";
-// import { Player } from "./models/player";
+import { Gameboard } from './models/gameboard';
+import { Ship } from './models/ship';
+import { Player } from './models/player';
 
-// const player1 = new Player();
-// const player2 = new Player();
+const initGame = (function () {
+  const player = new Player();
+  const computer = new Player(true);
+})();
